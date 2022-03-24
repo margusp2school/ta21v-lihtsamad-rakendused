f69b08f4529d32ecf31a9f6d7733c68726b9d887
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-24 11:18:56 +0200

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -5,42 +5,73 @@ if next_position == "1":
     print(". . .")
     print(". . .")
 
-if next_position == "2":
+    next_position = input("Please type what position: ")
+
+    if next_position == "1":
+        print("CANT DO THE MOVE")
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+
+        next_position = input("Please type what position: ")
+
+        if next_position == "1":
+            print("CANT DO THE MOVE")
+
+        elif next_position == "2":
+            print("CANT DO THE MOVE")
+
+        elif next_position == "3":
+            print("x o x")
+            print(". . .")
+            print(". . .")
+
+
+
+    elif next_position == "3":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+
+elif next_position == "2":
     print(". x .")
     print(". . .")
     print(". . .")    
 
-if next_position == "3":
+elif next_position == "3":
     print(". . x")
     print(". . .")
     print(". . .")    
 
-if next_position == "4":
+elif next_position == "4":
     print(". . .")
     print("x . .")
     print(". . .")       
 
-if next_position == "5":
+elif next_position == "5":
     print(". . .")
     print(". x .")
     print(". . .")       
 
-if next_position == "6":
+elif next_position == "6":
     print(". . .")
     print(". . x")
     print(". . .")       
 
-if next_position == "7":
+elif next_position == "7":
     print(". . .")
     print(". . .")
     print("x . .")       
 
-if next_position == "8":
+elif next_position == "8":
     print(". . .")
     print(". . .")
     print(". x .")       
 
-if next_position == "9":
+elif next_position == "9":
     print(". . .")
     print(". . .")
     print(". . x")       
