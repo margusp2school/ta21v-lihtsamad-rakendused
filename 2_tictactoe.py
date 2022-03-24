1213ebd38a23aca77bf30a22a5fbb02d0f9740ae
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-24 11:42:54 +0200

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -1,77 +1,23 @@
+game = [
+    ".", ".", ".",
+    ".", ".", ".",
+    ".", ".", "."
+]
+
+print(game[0] + game[1] + game[2])
+print(game[3] + game[4] + game[5])
+print(game[6] + game[7] + game[8])
+
 next_position = input("Please type what position: ")
 
+# line 14 and 17 are same. Please make "1" and "2" to int (using int() function and -1)
 if next_position == "1":
-    print("x . .")
-    print(". . .")
-    print(". . .")
+    game[0] = "X"
 
-    next_position = input("Please type what position: ")
+if next_position == "2":
+    game[1] = "X"    
 
-    if next_position == "1":
-        print("CANT DO THE MOVE")
-
-    elif next_position == "2":
-        print("x o .")
-        print(". . .")
-        print(". . .")
-
-        next_position = input("Please type what position: ")
-
-        if next_position == "1":
-            print("CANT DO THE MOVE")
-
-        elif next_position == "2":
-            print("CANT DO THE MOVE")
-
-        elif next_position == "3":
-            print("x o x")
-            print(". . .")
-            print(". . .")
-
-
-
-    elif next_position == "3":
-        print("x . o")
-        print(". . .")
-        print(". . .")
-
-
-elif next_position == "2":
-    print(". x .")
-    print(". . .")
-    print(". . .")    
-
-elif next_position == "3":
-    print(". . x")
-    print(". . .")
-    print(". . .")    
-
-elif next_position == "4":
-    print(". . .")
-    print("x . .")
-    print(". . .")       
-
-elif next_position == "5":
-    print(". . .")
-    print(". x .")
-    print(". . .")       
-
-elif next_position == "6":
-    print(". . .")
-    print(". . x")
-    print(". . .")       
-
-elif next_position == "7":
-    print(". . .")
-    print(". . .")
-    print("x . .")       
-
-elif next_position == "8":
-    print(". . .")
-    print(". . .")
-    print(". x .")       
-
-elif next_position == "9":
-    print(". . .")
-    print(". . .")
-    print(". . x")       
+# this is same as on line 7. make this a function
+print(game[0] + game[1] + game[2])
+print(game[3] + game[4] + game[5])
+print(game[6] + game[7] + game[8])
