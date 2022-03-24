a0d44e55c9a20e877c8d1249aa85a90bc7ff5471
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-24 10:52:15 +0200

    added calculator

---
+++ 1_calculator.py
@@ -0,0 +1,11 @@
+operation = input("Please write + or - to pick calculator: ")
+
+if operation == "+":
+    number1 = int(input("Please enter nr 1: "))
+    number2 = int(input("Please enter nr 2: "))
+    print(number1 + number2)
+
+if operation == "-":
+    number1 = int(input("Please enter nr 1: "))
+    number2 = int(input("Please enter nr 2: "))
+    print(number1 - number2)
--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 This is my first GIT project.
 
-![This is cat](https://raw.githubusercontent.com/margusp2school/ta21v-lihtsamad-rakendused/main/cat.jpg)
-
+![](https://raw.githubusercontent.com/margusp2school/ta21v-lihtsamad-rakendused/main/cat.jpg)
 
