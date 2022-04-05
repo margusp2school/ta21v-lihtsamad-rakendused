4e4766a832870f28d43824155f19e32838f9dd4d
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-04-05 08:30:47 +0300

    Update 3_laborinth.py

--- 3_laborinth.py
+++ 3_laborinth.py
@@ -1,9 +1,9 @@
 map = [
     [12, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 0],
-    [1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 24]
+    [1, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+    [24, 0, 1, 0, 0],
+    [1, 1, 1, 0, 24]
 ]
 
 start_pos_x = 0 
@@ -13,19 +13,26 @@ y_rows = 5
 x_columns = 5
 
 def get_next_free_position(current_position_y, current_position_x):
-    if current_position_x+1 < x_columns and map[current_position_y][current_position_x+1] == 1:
+    random_number = random.random()
+
+    can_go_right = current_position_x+1 < x_columns and map[current_position_y][current_position_x+1] == 1
+    can_go_left = current_position_x-1 > 0 and map[current_position_y][current_position_x-1] == 1
+    can_go_bottom = current_position_y+1 < y_rows and map[current_position_y+1][current_position_x] == 1
+    can_go_top = current_position_y-1 > 0 and map[current_position_y-1][current_position_x] == 1
+
+    if can_go_right:
         print("can go right")
         return [current_position_y, current_position_x+1]
 
-    if current_position_x-1 > 0 and map[current_position_y][current_position_x-1] == 1:
+    if can_go_left:
         print("can go left")
         return [current_position_y, current_position_x-1]
 
-    if current_position_y+1 < y_rows and map[current_position_y+1][current_position_x] == 1:
+    if can_go_bottom:
         print("can go bottom")
         return [current_position_y+1, current_position_x]
 
-    if current_position_y-1 > 0 and map[current_position_y-1][current_position_x] == 1:
+    if can_go_top:
         print("can go top")
         return [current_position_y-1, current_position_x]
 
